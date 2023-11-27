07f10dcd44b187f9148c64d77f66dfe309ac8058
henrizx <marcelohenrizx@gmail.com>
2023-11-27 15:14:46 -0300

    criando pecas, tabulerio

--- Xadrez_proje/Program.cs
+++ Xadrez_proje/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace Xadrez_proje
 {
@@ -7,10 +8,16 @@ namespace Xadrez_proje
     {
         static void Main(string[] args)
         {
-            Posicao P;
-            P = new Posicao(3, 4);
+            Tabuleiro tab = new Tabuleiro(8,8);
+            tab.colocarPeca(new Torre(tab, Cor.Preta) , new Posicao(0, 0));
+            tab.colocarPeca(new Rei(tab, Cor.Preta) , new Posicao(1, 3));
+            tab.colocarPeca(new Torre(tab,Cor.Preta) , new Posicao(2, 4));
 
-            Console.WriteLine(P);
+            Tela.imprimirTabuleiro(tab);
+
+
+            
+
 
             Console.ReadLine();
         }
---
+++ Xadrez_proje/Tela.cs
@@ -0,0 +1,29 @@
+﻿using tabuleiro;
+namespace Xadrez_proje
+{
+    class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+
+        {
+            /*os fors servem para criação das linhas e colunas do tabuleiro, a seguinte logica é aplicada:
+             * se a posicao i,j estiverem nulas de peças, então sobrepoõe com um traço, se não, coloca uma peça e um espaço
+            */
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (tab.peca(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(tab.peca(i, j) + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
