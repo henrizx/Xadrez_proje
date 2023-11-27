9045caa869664ac6798e484c1c450e3508489bbe
henrizx <marcelohenrizx@gmail.com>
2023-11-27 14:05:26 -0300

    iniciando projeto

---
+++ Xadrez_proje/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using tabuleiro;
+
+namespace Xadrez_proje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+            P = new Posicao(3, 4);
+
+            Console.WriteLine(P);
+
+            Console.ReadLine();
+        }
+    }
+}
